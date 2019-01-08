6aa845c24bf06eb0ccd47e42d6a255d0197bc7f9
Thompson Plyler <nealok.g@gmail.com>
2019-01-08 17:01:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here:
+// const result = words.filter(word => word.length > 6);
+// console.log(materials.map(material => material.length));
+
+// driversWithRevenueOver- This function takes an array of 
+// driver JavaScript objects as the first argument and a 
+// number called revenue as the second argument. It returns 
+// an array of driver objects that have a revenue attribute 
+// that's greater than the passed-in revenue argument.
+
+function driversWithRevenueOver(driver, revenue){
+    return driver.filter(thing => thing.revenue > revenue);
+}
+
+function driverNamesWithRevenueOver(driver, revenue){
+    collect =  driver.filter(thing => thing.revenue > revenue);
+    return collect.map(thing => thing.name);
+} 
+
+// Jesus's code (does work!) 
+function exactMatch(driver, obj) {
+   return driver.filter( (d) => d[Object.keys(obj)[0]] === obj[Object.keys(obj)[0]]);
+ }
+
+ function exactMatchToList(driver, obj){
+    return exactMatch(driver,obj).map(thing => thing.name); 
+    //list = driver.filter(d => d[Object.keys(obj)[0]]) === obj[Object.keys(obj)[0]];
+    //return list.map(element => element.name);
+}
+
+
+// My code (does not work!) 
+// function exactMatch(driver,{key: value}){
+//     return driver.filter(object => object.key === value); 
+// }
